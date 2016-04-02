97c3cf4b6d648ea02a51ab1eb6251a29b2f3ff5c
Heasummn <hassamuddin20@gmail.com>
2016-04-02 18:10:54 -0500

    (Cleanliness) Made recursion nicer and reformatted print statements

--- HTML_Parser/dom.py
+++ HTML_Parser/dom.py
@@ -21,20 +21,18 @@ class Dom(object):
         if self.root is not None:
             correct_type(root, Tag)
 
-    def get_tags(self, root, tags=None, recursive=True):
+    def get_tags(self, root, recursive=True):
         """
         Gets all child tags from a Tag object.
         :param root: Tag object, from where children are searched.
-        :param tags: tags list where found children are added
         :param recursive: boolean determining whether the search is recursive
         :return: list of tags
         """
-        if tags is None:
-            tags = []
+        tags = []
         for child in root.children:
             tags.append(child)
-            print "got here"
-            print len(tags)
+            print("got here")
+            print(len(tags))
             if recursive:
-                self.get_tags(child, tags)
+                tags += self.get_tags(child)
         return tags
