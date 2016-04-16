599ca8a9119984d873c0177e897b8d1ab4860149
xlog <efkaxlog@gmail.com>
2016-04-16 18:07:06 +0100

    add test method for html parsing

--- HTML_Parser/test.py
+++ HTML_Parser/test.py
@@ -1,9 +1,36 @@
-""" A module containing test doc stuff
+"""
+A module containing classes used for testing purposes
 """
 
-class Test(object):
-    """ This is an object
-    """
-    def test(self):
-        """ This is a test method
-        """
+import unittest
+from tag import Tag
+from parser import Parser
+from dom import Dom
+
+
+class TestParser(unittest.TestCase):
+
+    def test_html_string_parsing(self):
+        html = """
+                <html>
+                    <body>
+                        <p>This is a p tag</p>
+                    </body>
+                </html>
+               """
+        root = Tag("html")
+        body = Tag("body")
+        body.add_child(Tag("p"))
+        root.add_child(body)
+        test_dom = Dom(root=root)
+
+        parser = Parser(html)
+        parsed_dom = parser.parse_html()
+        parsed_tags = parsed_dom.get_tags(parsed_dom.root)
+        expected_tags = test_dom.get_tags(test_dom.root)
+        self.assertEquals(len(expected_tags), len(parsed_tags))
+        for i, tag in enumerate(expected_tags):
+            self.assertEquals(tag.name, parsed_tags[i].name)
+
+if __name__ == '__main__':
+    unittest.main()
