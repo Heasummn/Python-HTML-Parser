dbba72435d37c1dd876203bdd9f5c0b92d031f1e
xlog <efkaxlog@gmail.com>
2016-04-16 17:58:20 +0100

    Remove the Parser class dom attribute, have it be created in the parse_html() method and returned from there. Therefore also remove the call to the parse_html() method in the constructor. Make parse_html() public, sort out some comment formatting and remove print statements.

--- HTML_Parser/parser.py
+++ HTML_Parser/parser.py
@@ -17,39 +17,30 @@ class Parser(object):
         """
         self.html = html
         self.current_tag = None
-        self.dom = Dom()
-        self._parse_html()
 
-    def _parse_html(self):
+    def parse_html(self):
+        dom = Dom()
         index = 0
         while index <= len(self.html) - 1:
-            print(index)
             char = self.html[index]
-            #print(char)
             if char == "<":
                 if self.html[index + 1] == "/": # Ending a tag
-                    print("Closing Tag: " + self.current_tag.name)
                     self.current_tag.set_closed(True)
                     index = self.html.find(">", index) # Skip over to the end
                     self.current_tag = self.current_tag.parent
 
-                else: # It's just a normal tag guys, calm down
+                else:  # It's just a normal tag guys, calm down
                     index += 1
                     char = self.html[index]
-                    print("Opening Tag")
                     tag_name = ""
-                    while char != ">": # Fill up the name of the tag
+                    while char != ">":  # Fill up the name of the tag
                         tag_name += char
                         index += 1
                         char = self.html[index]
-                        #print(tag_name)
-                    print("Opened Tag: " + tag_name)
-                    if self.current_tag is None: # It's our first tag
+                    if self.current_tag is None:  # It's our first tag
                         self.current_tag = Tag(tag_name)
-                        self.dom.set_root(self.current_tag)
-                    else: # It's not our first tag
+                        dom.set_root(self.current_tag)
+                    else:  # It's not our first tag
                         self.current_tag = Tag(tag_name, self.current_tag)
-                    #print(self.current_tag.name)
             index += 1
-
-test = Parser("<html><body></body></html>")
+        return dom
